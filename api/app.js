647cd5c024121441990d2fa66bc2165cf5dc63e5
Haolong Li <fr20938@bristol.ac.uk>
2021-06-16 11:23:47 +0800

    Revert "Revert "Create and deploy access.log""
    This reverts commit 3aa94f8c0bfbd43355abf51d4a465080c8db0bb2.

--- api/app.js
+++ api/app.js
@@ -9,11 +9,15 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+const accessLog = fs.createWriteStream('./access.log', {flags : 'a'});
+app.use(logger('combined', {stream : accessLog}));
+// app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
