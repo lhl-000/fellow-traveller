1e114bd644ab67abc40877e414c549d701fe7cb7
Haolong Li <fr20938@bristol.ac.uk>
2021-06-23 17:00:11 +0800

    finish people page

--- client/src/pages/people/index.js
+++ client/src/pages/people/index.js
@@ -6,59 +6,59 @@ import Footer from './components/footer';
 import { useObserverHook } from '@/hooks';
 import { CommonEnum } from '@/enum';
 import { useDispatch, shallowEqual, useSelector } from 'react-redux';
-import { getDetailAsync, getCommentsAsync, resetData, reloadComments  } from '@/redux/actions/people';
+import { getDetailAsync, getCommentsAsync, resetData, reloadComments } from '@/redux/actions/people';
 import { useLocation } from 'react-router-dom';
-import  './index.scss';
+import './index.scss';
 
 
 export default function People() {
 
-    const selector = state => ({
-        detail: state.detail,
-        comments: state.comments,
-        page: state.page,
-        showLoading: state.showLoading,
-        reloadCommentsNum: state.reloadCommentsNum
-    });
+  const selector = state => ({
+    detail: state.detail,
+    comments: state.comments,
+    page: state.page,
+    showLoading: state.showLoading,
+    reloadCommentsNum: state.reloadCommentsNum
+  });
 
-    const { detail, comments, page , reloadCommentsNum, showLoading } = useSelector( selector, shallowEqual);
+  const { detail, comments, page, reloadCommentsNum, showLoading } = useSelector(selector, shallowEqual);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const { search } = useLocation();
+  const { search } = useLocation();
 
-    const query = new URLSearchParams(search);
+  const query = new URLSearchParams(search);
 
-    useObserverHook('#'+ CommonEnum.LONDING_ID, (entries) => {
-        if(comments && comments.length && showLoading && entries[0].isIntersecting){
-          dispatch(reloadComments({}));
-        }
-      }, [comments, showLoading]);
+  useObserverHook('#' + CommonEnum.LONDING_ID, (entries) => {
+    if (comments && comments.length && showLoading && entries[0].isIntersecting) {
+      dispatch(reloadComments({}));
+    }
+  }, [comments, showLoading]);
 
-      useEffect(() => {
-        dispatch(getDetailAsync({
-          id: query?.get('id')
-        }));
-      }, []);
-    
-      useEffect(()=>{
-        dispatch(getCommentsAsync({page, comments},{
-          id: query?.get('id')
-        }));
-      }, [reloadCommentsNum]);
-    
-      useEffect(()=>{
-        return () => {
-            dispatch(resetData({}));
-        }
-      }, []);
+  useEffect(() => {
+    dispatch(getDetailAsync({
+      id: query?.get('id')
+    }));
+  }, []);
 
-    return (
-        <div className='people-page'>
-            <Banner />
-            <Info detail={detail}/>
-            <List lists={comments} showLoading={showLoading}/>
-            <Footer />
-        </div>
-    )
+  useEffect(() => {
+    dispatch(getCommentsAsync({ page, comments }, {
+      id: query?.get('id')
+    }));
+  }, [reloadCommentsNum]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetData({}));
+    }
+  }, []);
+
+  return (
+    <div className='people-page'>
+      <Banner />
+      <Info detail={detail} />
+      <List lists={comments} showLoading={showLoading} />
+      <Footer />
+    </div>
+  )
 }
--- client/src/pages/user/index.js
+++ client/src/pages/user/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { List } from 'antd-mobile';
+
 export default function User() {
     return (
-        <div>
-            user
+        <div className='user-page'>
+            <div className='info'>
+                <div className='set' onClick={handleClick}>Setting</div>
+                <div className='user'>
+                    <img alt='user' src={avatar} />
+                    <div className='tel'>{tel}</div>
+                    <div className='sign'>{sign}</div>
+                </div>
+            </div>
+            <div className='lists'>
+                <List>
+                    <List.Item arrow='horizontal'>
+                        用户协议
+                    </List.Item>
+                    <List.Item arrow='horizontal'>
+                        常见问题
+                    </List.Item>
+                    <List.Item arrow='horizontal'>
+                        联系客服
+                    </List.Item>
+                </List>
+            </div>
         </div>
     )
 }
