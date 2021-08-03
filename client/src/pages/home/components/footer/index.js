8d17ce3caa4ef93f3e496e920e5841dfa96a254b
Haolong Li <fr20938@bristol.ac.uk>
2021-08-03 12:19:22 +0800

    fix cpyright

--- client/src/pages/home/components/footer/index.js
+++ client/src/pages/home/components/footer/index.js
@@ -3,8 +3,12 @@ import React from 'react'
 export default function footer() {
     return (
         <div className='footer'>
+            <div className='cpyright'>
+            &nbsp;&nbsp;&nbsp;&nbsp;
             ©2021 Haolong Li<br/>
             渝ICP备2021007804号-1
+            </div>
+
         </div>
     )
 }
